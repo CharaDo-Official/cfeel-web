ce9330136d935329db7bf092a470393c28b1c9cf
tipsy <tipsy@email.com>
2025-11-27 10:05:45 +0900

    #1 add フッター追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from '@/components/layout/Header';
+import { Footer } from '@/components/layout/Footer';
 import { Home } from '@/pages/Home';
 import { CharaDoHome } from '@/pages/charado/CharaDoHome';
 import { Character } from '@/pages/charado/Character';
@@ -20,6 +21,7 @@ function App() {
 				<Route path="/charado/plan" element={<Plan />} />
 				<Route path="/charado/policy" element={<Policy />} />
 			</Routes>
+			<Footer />
 		</Router>
 	);
 }
